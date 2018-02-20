311675f60fe31ff200a274f458f24be4fde8fe28
Mohamed Samassi <msamassi@gmail.com>
2018-02-20 10:14:40 +0100

    RealmSwift - Searchbar - search by title : failed

---
+++ Todoey/Data Model/Category.swift
@@ -0,0 +1,9 @@
+//
+//  Category.swift
+//  Todoey
+//
+//  Created by Mohamed Samassi on 16/02/2018.
+//  Copyright © 2018 Mo Samassi. All rights reserved.
+//
+
+import Foundation
---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,9 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Mohamed Samassi on 16/02/2018.
+//  Copyright © 2018 Mo Samassi. All rights reserved.
+//
+
+import Foundation
